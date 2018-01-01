69a72849f052f9cbb6f384540574a1222261d187
Rodion Staroverov <rodion.staroverov@gmail.com>
2018-01-02 03:35:38 +0400

    Init commit.

---
+++ components/LoginComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, TextInput, Text } from 'react-native';
+
+export default class LoginComponent extends Component {
+    render() {
+        return (
+          <View>
+            <Text>Login</Text>
+          </View>
+        );
+    }
+}
